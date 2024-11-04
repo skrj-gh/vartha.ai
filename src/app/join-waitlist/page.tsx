b014bc6872204c50f203a5e5e4f3dd25fc61b401
skrj_ <shubhkarjha533@gmail.com>
2024-11-04 22:10:08 +0530

    join-waitlist done

--- src/app/join-waitlist/page.tsx
+++ src/app/join-waitlist/page.tsx
@@ -4,11 +4,15 @@ import Left_section from '@/components/left_section';
 import React from 'react'
 import Image from 'next/image';
 import send_btn from "../assets/send_arrow.png"
+import close from "../assets/close.svg"
 import "./join-waitlist.css"
 
 export default function Join_waitlist() {
   return (
     <div className='bg-white flex flex-row join_container'>
+      <div className='closeDiv'>
+        <Image src={close} alt='close' width={20}/>
+      </div>
       <div className='left_section'>
         <Left_section/>
       </div>
@@ -19,13 +23,13 @@ export default function Join_waitlist() {
           <span className="country-code">+91</span>
           <input type="text" placeholder="Enter your number..."/>
           <button className="send-button">
-            <Image src={send_btn} alt="Send" style={{width: "2vw", height:"2vw"}}/>
+            <Image src={send_btn} alt="Send" style={{width: "24px", height:"24px"}}/>
           </button>
         </div>
         <p>You&apos;ll be recieving a code on this number!</p>
-        <a href="/verify-otp"><button className='flex flex-row items-center gap-3 cont_btn'>
+        <a href="/verify-otp"><button className='flex flex-row items-center gap-[8px] cont_btn'>
           Continue
-          <Image src={send_btn} alt='send' width={25}></Image>
+          <Image src={send_btn} alt='send' width={20}></Image>
         </button></a>
       </div>
     </div>
--- src/components/left_section.tsx
+++ src/components/left_section.tsx
@@ -13,7 +13,7 @@ export default function Left_section() {
     ];
 
     return (
-    <div className='left_div flex flex-col justify-between'>
+    <div className='left_div flex flex-col justify-between relative'>
       <nav className='left_nav'>
         <div className="flex flex-row justify-between items-center" style={{width: "7.5vw"}}>
           <Image src={logo} alt="head" style={{height: "5vw", width: "5vw", borderRadius: "50%"}}></Image>
@@ -39,17 +39,17 @@ export default function Left_section() {
       </div>
 
       {/* Dots Navigation */}
-      <div className="flex justify-start mt-4 ml-4 absolute bottom-0 dot_nav_left">
+      <div className="flex justify-start absolute bottom-0 dot_nav_left px-[8px] gap-[12px]">
         {items.map((item, index) => (
             <div onClick={() => setCurrentIndex(index)} className='cursor-pointer' key={index}>
 
                 <button
-                className={`w-[9vw] h-0.5 rounded-full transition-all ${
-                    currentIndex === index ? 'bg-gray-800 w-5' : 'bg-gray-300'
+                className={`w-[110px] h-0.5 rounded-full transition-all ${
+                    currentIndex === index ? 'bg-[#2D2F3C]' : 'bg-[#2D2F3C] opacity-40'
                 }`}
                 />
-                <p className={`text-xs text-center w-[5vw] transition-all ${
-                currentIndex === index ? 'text-gray-800 w-5' : 'text-gray-300'
+                <p className={`w-[110px] text-xs text-start font-semibold transition-all ${
+                currentIndex === index ? 'text-[#2D2F3C]' : 'text-[#2D2F3C] opacity-40'
                 }`}>{item.title}</p>
             </div>
         ))}
